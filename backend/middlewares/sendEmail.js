ec85027bde60b55bc56d08c0d698f6fc01a744f2
Sourabh <sourabhdas746@gmail.com>
2022-03-31 13:03:50 +0530

    first commit

---
+++ backend/middlewares/sendEmail.js
@@ -0,0 +1,21 @@
+const nodeMailer = require("nodemailer");
+
+exports.sendEmail = async (options) => {
+  const transporter = nodeMailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "46d31e38b5d618",
+      pass: "626194d7ba7343",
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.SMPT_MAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
